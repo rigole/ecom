258b5464a47c74510012408fcf93d7ac15fd3b39
rigole <foplacide@gmail.com>
2021-11-06 20:31:25 +0100

    Image serialization and setting the product view

--- api/product/views.py
+++ api/product/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .serializers import ProductSerializer
+from rest_framework import viewsets
+from .models import Product
 # Create your views here.
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('id')
+    serializer_class = ProductSerializer
