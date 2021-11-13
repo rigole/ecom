716f208081cd48fb3945b2675f6e190d21257af3
rigole <foplacide@gmail.com>
2021-11-13 20:32:23 +0100

    fixing super admin issue

---
+++ api/migrations/0001_initial.py
@@ -0,0 +1,18 @@
+from django.db import migrations
+from api.user.models import CustomUser
+
+
+class Migration(migrations.Migration):
+    def seed_data(apps, schema_editor):
+        user = CustomUser(name="plass", email="foplacide@gmail.com",is_staff=True, is_superuser=True,
+                          phone="67800784", gender="Male")
+        user.set_password("123")
+        user.save()
+
+        dependencies = [
+
+        ]
+
+    operations = [
+        migrations.RunPython(seed_data),
+    ]
