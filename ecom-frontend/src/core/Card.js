5559c5846180f57dba4cf653279fe07173021f24
rigole <foplacide@gmail.com>
2021-11-25 07:37:55 +0100

    text from backend

--- ecom-frontend/src/core/Card.js
+++ ecom-frontend/src/core/Card.js
@@ -7,38 +7,40 @@ const Card = ({
                   addToCart = true,
                   removeFromCart = false
               }) => {
+         const cartTitle = product ? product.name : "A photo pexels"
+         const cartDescription = product ? product.description : "A photo pexels"
+         const cartPrice = product ? product.price : "Default"
+        return (
+            <div className="card text-white bg-dark border border-info">
+                <div className="card-header lead">{cartTitle}</div>
+                <div className="card-body">
+                  <ImageHelper product={product}/>
+                    <p className="lead bg-success font-weight-normal text-wrap">
+                        {cartDescription}
+                    </p>
+                    <p className="btn btn-success rounded btn-sm px-4">{cartPrice} XAF</p>
+                    <div className="row">
+                        <div className="col-12">
+                            <button
+                                onClick={() => {}}
+                                className="btn btn-block btn-outline-success mt-2 mb-2"
+                            >
+                                Add to Cart
 
-    return (
-        <div className="card text-white bg-dark border border-info">
-            <div className="card-header lead">A photo from pexels</div>
-            <div className="card-body">
-              <ImageHelper product={product}/>
-                <p className="lead bg-success font-weight-normal text-wrap">
-                    this photo looks great
-                </p>
-                <p className="btn btn-success rounded btn-sm px-4">XAF 12500</p>
-                <div className="row">
-                    <div className="col-12">
-                        <button
-                            onClick={() => {}}
-                            className="btn btn-block btn-outline-success mt-2 mb-2"
-                        >
-                            Add to Cart
-
-                        </button>
-                    </div>
-                    <div className="col-12">
-                        <button
-                            onClick={() => {}}
-                            className=" btn btn-block btn-outline-danger mt-2 mb-2"
-                        >
-                            Remove from Cart
-                        </button>
+                            </button>
+                        </div>
+                        <div className="col-12">
+                            <button
+                                onClick={() => {}}
+                                className=" btn btn-block btn-outline-danger mt-2 mb-2"
+                            >
+                                Remove from Cart
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
 }
 export default Card
 
