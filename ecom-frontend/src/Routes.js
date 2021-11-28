e80a70a4bec77523919072564557df1e12cd5142
rigole <foplacide@gmail.com>
2021-11-28 16:38:45 +0100

    handling privates routing

--- ecom-frontend/src/Routes.js
+++ ecom-frontend/src/Routes.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Home from "./core/Home";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
+import PrivateRoutes from "./auth/helper/PrivateRoutes";
 
 const Routs = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/"  element={<Home/>} />
+                {/*<PrivateRoutes path="/user/dashboard" exact component={}/>*/}
             </Routes>
         </BrowserRouter>
     )
