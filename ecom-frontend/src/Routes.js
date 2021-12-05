2a38cb27ccb0cff025e319fe6d453b94fe1ebd05
rigole <foplacide@gmail.com>
2021-12-05 05:44:57 +0100

    Getting the signin form ready

--- ecom-frontend/src/Routes.js
+++ ecom-frontend/src/Routes.js
@@ -4,15 +4,16 @@ import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Signup from "./user/Signup";
 import PrivateRoutes from "./auth/helper/PrivateRoutes";
 import UserDashboard from "./user/UserDashboard";
+import Signin from "./user/Signin";
 
 const Routs = () => {
     return (
         <BrowserRouter>
 
             <Routes>
-
                 <Route path="/"  element={<Home/>} />
                 <Route path="/signup" exact element={<Signup/>} />
+                <Route path="/signin" exact element={<Signin/>} />
                 <Route eaxct path="/user/dashboard" element ={<PrivateRoutes/>}>
                       <Route exact path="/user/dashboard" element={<UserDashboard/>}/>
                 </Route>
--- ecom-frontend/src/user/Signin.js
+++ ecom-frontend/src/user/Signin.js
@@ -1,12 +1,110 @@
 import Base from "../core/Base";
-import React from "react";
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
 
 const Signin = () => {
 
+    const [values, setValues] = useState({
+        name:"",
+        email:"",
+        password:"",
+        error:"",
+        success: false,
+        loading: false,
+        didRedirect: false
+    })
+
+
+    const { name, email, password, error, success, loading, didRedirect } = values
+    const handleChange = (name) =>
+        (event) => {
+            setValues({...values, error:false, [name]: event.target.value })
+        }
+
+     const successMessage = () => {
+            return (
+                <div className="row">
+                    <div className="col-md-6 offset-sm-3 text-left">
+                        <div
+                            className="alert alert-success"
+                            style={{display: success ? "" : "none" }}
+
+                        >
+                              New account created successfully. Please
+                            <Link to="/signin">login now</Link>
+
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+
+     const errorMessage = () => {
+            return (
+                <div className="row">
+                    <div className="col-md-6 offset-sm-3 text-left">
+                        <div
+                            className="alert alert-danger"
+                            style={{display: error ? "" : "none" }}
+
+                        >
+                              Error while creating new user. Check fields again and Please try again
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+
+     const SignInForm = () => {
+            return(
+                <div className="row">
+                    <div className="col-md-6 offset-sm-3 text-left">
+                        <form action="">
+
+                            <div className="form-group">
+                                <label className="text-light">Email</label>
+                                <input
+                                    className="form-control"
+                                    value={email}
+                                    onChange={handleChange("email")}
+                                    type="email"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label className="text-light">password</label>
+                                <input
+                                    className="form-control"
+                                    value={password}
+                                    onChange={handleChange("password")}
+                                    type="password"
+                                />
+                            </div>
+                            <button
+                                className="btn btn-success btn-block"
+                                onClick={() => {}}
+                            >
+                                Sign In
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            )
+        }
+
+
+
+
+
+
+
+
     return (
-        <div>
-            <h1>Welcome to Sign</h1>
-        </div>
+        <Base title=" Welcome to sign in page" description=" A t-shirt store">
+            {SignInForm()}
+            <p className="text-center">
+                {JSON.stringify(values)}
+            </p>
+        </Base>
     )
 }
 
