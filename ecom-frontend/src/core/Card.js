78609edda55d14403b213d3f5f5f593ee0b007c3
rigole <foplacide@gmail.com>
2021-12-25 11:16:58 +0100

    deleting from cart

--- ecom-frontend/src/core/Card.js
+++ ecom-frontend/src/core/Card.js
@@ -5,85 +5,89 @@ import {addItemToCart, removeItemFromCart} from "./helperJS/cartHelper";
 import { isAuthenticated } from "../auth/helper";
 
 const Card = ({
-                  product,
-                  addToCart1 = true,
-                  removeFromCart = false,
-              }) => {
+      product,
+      addToCart1 = true,
+      removeFromCart = false,
+      reload = undefined,
+      setReload = f => f,
+
+      }) => {
 
 
-        const [redirect, setRedirect] = useState(false)
+    const [redirect, setRedirect] = useState(false)
 
-         const cartTitle = product ? product.name : "A photo pexels"
-         const cartDescription = product ? product.description : "A photo pexels"
-         const cartPrice = product ? product.price : "Default"
+     const cartTitle = product ? product.name : "A photo pexels"
+     const cartDescription = product ? product.description : "A photo pexels"
+     const cartPrice = product ? product.price : "Default"
 
-        const addToCart = () => {
-          if (isAuthenticated()) {
-              addItemToCart(product, ()=> setRedirect(true))
-                console.log("Added to cart")
-          } else {
-              console.log("Login Please!")
-          }
-        }
+    const addToCart = () => {
+      if (isAuthenticated()) {
+          addItemToCart(product, ()=> setRedirect(true))
+            console.log("Added to cart")
+      } else {
+          console.log("Login Please!")
+      }
+    }
 
-        const getARedirect = (redirect) => {
-             if (redirect) {
-                 return <Navigate to="/cart"/>
-             }
-        }
+    const getARedirect = (redirect) => {
+         if (redirect) {
+             return <Navigate to="/cart"/>
+         }
+    }
 
-        const showAddToCart = addToCart => {
-             return(
-                    addToCart && (
-                        <button
-                            onClick={addToCart}
-                            className="btn btn-block btn-outline-success mt-2 mb-2"
-                        >
-                                Add to Cart
+    const showAddToCart = addToCart => {
+         return(
+                addToCart && (
+                    <button
+                        onClick={addToCart}
+                        className="btn btn-block btn-outline-success mt-2 mb-2"
+                    >
+                            Add to Cart
 
-                            </button>
-                    )
+                        </button>
+                )
+         )
+    }
+
+    const showRemoveFromCart = (removeFromCart) => {
+         return (
+             removeFromCart && (
+                  <button
+                  onClick={() => {
+                      removeItemFromCart(product._id)
+                      setReload(!reload)
+                      console.log("Product removed from cart ")
+                  }}
+                  className=" btn btn-block btn-outline-danger mt-2 mb-2"
+              >
+                            Remove from Cart
+
+              </button>
              )
-        }
 
-        const showRemoveFromCart = (removeFromCart) => {
-             return (
-                 removeFromCart && (
-                      <button
-                      onClick={() => {
-                          removeItemFromCart(product._id)
-                          console.log("Product removed from cart ")
-                      }}
-                      className=" btn btn-block btn-outline-danger mt-2 mb-2"
-                  >
-                                Remove from Cart
-
-                  </button>
-                 )
-
-             )
-        }
-        return (
-            <div className="card text-white bg-dark border border-info">
-                <div className="card-header lead">{cartTitle}</div>
-                <div className="card-body">
-                    {getARedirect(redirect)}
-                  <ImageHelper product={product}/>
-                    <p className="lead bg-success font-weight-normal text-wrap">
-                        {cartDescription}
-                    </p>
-                    <p className="btn btn-success rounded btn-sm px-4">{cartPrice} XAF</p>
-                    <div className="row">
-                        <div className="col-12">
-                            {showAddToCart(addToCart)}
-                        </div>
-                        <div className="col-12">
-                             {showRemoveFromCart(removeFromCart)}
-                        </div>
+         )
+    }
+    return (
+        <div className="card text-white bg-dark border border-info">
+            <div className="card-header lead">{cartTitle}</div>
+            <div className="card-body">
+                {getARedirect(redirect)}
+              <ImageHelper product={product}/>
+                <p className="lead bg-success font-weight-normal text-wrap">
+                    {cartDescription}
+                </p>
+                <p className="btn btn-success rounded btn-sm px-4">{cartPrice} XAF</p>
+                <div className="row">
+                    <div className="col-12">
+                        {showAddToCart(addToCart)}
+                    </div>
+                    <div className="col-12">
+                         {showRemoveFromCart(removeFromCart)}
                     </div>
                 </div>
             </div>
-        )
+        </div>
+    )
 }
 export default Card
 
